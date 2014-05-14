b9a0c8af792dd5ab381c4eb2114a8845549d08b4
Sharikul Islam <s.islam73@ymail.com>
2014-05-14 09:40:40 +0100

    Update Scope.js

--- Scope.js
+++ Scope.js
@@ -1,1 +1,48 @@
-s
+(function() {
+  
+  var scopes = {},
+      forEach = [].forEach.call;
+  
+  
+  function defineScope(scope, link) {
+    if(!scopes[scope]) {
+     scopes[scope] = link;
+     return true;
+    }
+    
+    return false;
+  }
+  
+  function runScope(scope) {
+    if(scopes[scope]) {
+      var eScope = document.querySelectorAll('[data-scope=' + scope + ']');
+      
+      if(eScope.length > 0) {
+        forEach(eScope, function(Element) {
+          var All = Element.querySelectorAll('*');
+          
+          if(All.length > 0) {
+            forEach(All, function(subElement) {
+              if(subElement.innerHTML.match(/{{(.*?)}}/)) {
+                subElement.innerHTML = subElement.innerHTML.replace(/{{(.*?)}}/g, function(Raw, Key) {
+                  return scopes[scope][Key] ? scopes[scope][Key]: Raw;
+                });
+              }
+            });
+          }
+        });
+      }
+    }
+  }
+  
+  window.Scope = {
+    defineScope: defineScope,
+    runScope: runScope;
+  }
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    forEach(scopes, function(scope) {
+      runScope(scope);
+    });
+  });
+})();
